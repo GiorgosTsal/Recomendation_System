55e69cd8dc8d7074fe7d8c0d2b0f03626bbad67e
Giorgos Tsalidis <tsalgiwrgos@gmail.com>
2018-05-29 04:02:13 +0300

    Add files via upload

--- src/project_recomendation_system/recomendation_system.java
+++ src/project_recomendation_system/recomendation_system.java
@@ -4,19 +4,8 @@ package project_recomendation_system;
 
 
 import java.awt.EventQueue;
-import java.awt.FlowLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JSlider;
-import javax.swing.JSpinner;
-import javax.swing.SpinnerNumberModel;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-import javax.swing.JLabel;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.LayoutStyle.ComponentPlacement;
-import java.awt.GridLayout;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -24,41 +13,30 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-import javax.swing.JTextField;
-import javax.swing.JTextPane;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
-/** @see https://stackoverflow.com/questions/6067898 */
+
 public class recomendation_system extends JPanel {
 
     /**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-    private JTextField textField_T;
-    private JTextField textField_N;
-    private JTextField textField_X;
-    private JTextField textField_M;
-    private JTextField textField_K;
 
     
-    
-    Data_Matrix data ;
-    private JTextPane textPane;
+    static GUI gui;
+    static Data_Matrix data,data_predicted ;
+    static int T,N,M,X,K;
     
 	public  static void main(String args[]) {
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
-                JFrame f = new JFrame("SpinSlider!");
-                f.getContentPane().add(new recomendation_system());
+            	recomendation_system project = new recomendation_system();
+                JFrame f = new JFrame("recomendation system");
+                f.getContentPane().add(project.gui);
                 f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 f.pack();
                 f.setVisible(true);
-                String UserDir = System.getProperty("user.dir");
-                System.out.println(UserDir);
             }
         });
     }
@@ -67,15 +45,90 @@ public class recomendation_system extends JPanel {
 	
 	
 	public recomendation_system() {
-       
-    	init_components();
-
-
+		gui = new GUI();
 
     }
 
+	
+	public static void find_all_neighbors(){
+		Nearest_Neighbor[] all_neighbors = new Nearest_Neighbor[data.N];
+		for(int j=0; j<data.N; j++){
+			all_neighbors[j] = new Nearest_Neighbor(data.M, "max");
+			all_neighbors[j] = compute_cosine(j);
+		}
+		
+//		float sum = 0;
+//		all_neighbors[0].print_matrix();
+//        for(int i=0; i< data.M; i++){
+//            if(data.data[i][0]==-1){
+//            	System.out.println("found null at : "+ i);
+//            	for(int k=0; k<K; k++){
+//            		sum = sum + all_neighbors[0].matrix[k].value*data.data[i][all_neighbors[0].matrix[k].index];
+//            	}
+//            	sum = sum / all_neighbors[0].get_sum_similaty();
+//            	System.out.println("new value: " + sum );
+//            }
+//            
+//        }
+		///////////////////////////////
+        for(int i=0; i< data.M; i++){
+        	for(int j=0; j<data.N; j++){
+        		data_predicted.data[i][j] = data.data[i][j];
+        		if(data.data[i][j]==-1){
+        			data_predicted.data[i][j] = (int)(find_predicted_val(all_neighbors[j], data, i, j));
+        		}
+        	}
+        }
+        data_predicted.print_data_matrix();
+        
+	}
+	
+	public static float find_predicted_val(Nearest_Neighbor geitones , Data_Matrix data , int i , int j){
+		float sum = 0;
+		float sum_sim=0;
+		for(int k=0; k<geitones.size; k++){
+			if(data.data[i][geitones.matrix[k].index]!=-1){
+				sum = sum + geitones.matrix[k].value*data.data[i][geitones.matrix[k].index];
+				sum_sim = sum_sim + geitones.matrix[k].value;
+			}
+		}
+		
+		
+		float val = sum / sum_sim;
+		return val ;
+	}
+	
+	public static Nearest_Neighbor compute_cosine(int index_vector){
+		int howMany = K;
+        Nearest_Neighbor neighbors = new Nearest_Neighbor(howMany, "max");
+        double[] vector1,vector2;
+        vector1 = new double[data.M];
+        vector2 = new double[data.M];
+        
+        
+        
+        for(int i=0; i< data.M; i++){
+            vector1[i] = data.data[i][index_vector];
+            
+        }
+        
+        for(int j=0; j<data.N; j++){ // find all similaritis 
+        	
+        	if(index_vector!=j ){
+	            for(int i=0; i< data.M; i++){
+	                vector2[i] = data.data[i][j];
+	            
+	            } 
+	            double val = Cosine_similarity.compute(vector1, vector2);
+	            neighbors.add_new_value(j, (float)val);
+	
+	        }
+        }
+//        neighbors.print_matrix();
+        return neighbors ;
+	}
 
-    public void readTXT(){
+    public static void readTXT(){
     	
     	String path = System.getProperty("user.dir");
     	path = path + "/CONFIGFILE.TXT";
@@ -92,12 +145,12 @@ public class recomendation_system extends JPanel {
 								
 				input_data = fileIn.nextLine() ;
 				switch(input_data.charAt(0)){
-					
-				case 'T' : textField_T.setText(input_data.substring(1, input_data.length())); break ;
-				case 'N' : textField_N.setText(input_data.substring(1, input_data.length())); break ;
-				case 'M' : textField_M.setText(input_data.substring(1, input_data.length())); break ;
-				case 'X' : textField_X.setText(input_data.substring(1, input_data.length())+"%"); break ;
-				case 'K' : textField_K.setText(input_data.substring(1, input_data.length())); break ;
+		
+				case 'T' : T = Integer.parseInt(input_data.substring(1, input_data.length())); break ;
+				case 'N' : N = Integer.parseInt(input_data.substring(1, input_data.length())); break ;
+				case 'M' : M = Integer.parseInt(input_data.substring(1, input_data.length())); break ;
+				case 'X' : X = Integer.parseInt(input_data.substring(1, input_data.length())); break ;
+				case 'K' : K = Integer.parseInt(input_data.substring(1, input_data.length())); break ;
 					
 				}
 			}
@@ -113,125 +166,36 @@ public class recomendation_system extends JPanel {
     }
     
     
-    public void setTxt(){
+    public static void setTxt(){
     	String path = System.getProperty("user.dir");
     	path = path + "/CONFIGFILE.TXT";
         File logFile = new File(path);
 
         try {
 			BufferedWriter writer = new BufferedWriter(new FileWriter(logFile));
-			writer.write("T" + textField_T.getText()+"\n");
-			writer.write("N" + textField_N.getText()+"\n");
-			writer.write("M" + textField_M.getText()+"\n");
-			writer.write("X" + textField_X.getText().substring(0,textField_X.getText().length()-1)+"\n");
-			writer.write("K" + textField_K.getText()+"\n");
+			writer.write("T" + gui.textField_T.getText()+"\n");
+			writer.write("N" + gui.textField_N.getText()+"\n");
+			writer.write("M" + gui.textField_M.getText()+"\n");
+			writer.write("X" + gui.textField_X.getText().substring(0,gui.textField_X.getText().length()-1)+"\n");
+			writer.write("K" + gui.textField_K.getText()+"\n");
 			writer.close(); 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-
     }
-
     
-    public void init_components(){
-
-
-         
-         JPanel panel = new JPanel();
-
-         textPane = new JTextPane();
-         GroupLayout groupLayout = new GroupLayout(this);
-         groupLayout.setHorizontalGroup(
-         	groupLayout.createParallelGroup(Alignment.LEADING)
-         		.addGroup(groupLayout.createSequentialGroup()
-         			.addContainerGap()
-         			.addComponent(panel, GroupLayout.PREFERRED_SIZE, 159, GroupLayout.PREFERRED_SIZE)
-         			.addGap(29)
-         			.addComponent(textPane, GroupLayout.DEFAULT_SIZE, 412, Short.MAX_VALUE)
-         			.addGap(28))
-         );
-         groupLayout.setVerticalGroup(
-         	groupLayout.createParallelGroup(Alignment.LEADING)
-         		.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
-         			.addContainerGap()
-         			.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-         				.addComponent(panel, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 229, Short.MAX_VALUE)
-         				.addComponent(textPane, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 229, Short.MAX_VALUE))
-         			.addGap(150))
-         );
-         panel.setLayout(new GridLayout(0, 2, 0, 0));
-         
-         JLabel lblInputData = new JLabel("input data");
-         panel.add(lblInputData);
-         
-         JLabel lblNewLabel_1 = new JLabel("New label");
-         panel.add(lblNewLabel_1);
-         
-         JLabel label = new JLabel("T:");
-         panel.add(label);
-         
-         textField_T = new JTextField();
-         panel.add(textField_T);
-         textField_T.setColumns(10);
-         
-         JLabel lblNewLabel = new JLabel("N:");
-         panel.add(lblNewLabel);
-         
-         textField_N = new JTextField();
-         panel.add(textField_N);
-         textField_N.setColumns(10);
-         
-         JLabel lblM = new JLabel("M");
-         panel.add(lblM);
-         
-         textField_M = new JTextField();
-         panel.add(textField_M);
-         textField_M.setColumns(10);
-         
-         JLabel lblX = new JLabel("X:");
-         panel.add(lblX);
-         
-         textField_X = new JTextField();
-         panel.add(textField_X);
-         textField_X.setColumns(10);
-         
-         JLabel lblK = new JLabel("K:");
-         panel.add(lblK);
-         
-         textField_K = new JTextField();
-         panel.add(textField_K);
-         textField_K.setColumns(10);
-         
-         JButton btnNewButton = new JButton("set values");
-         btnNewButton.addActionListener(new ActionListener() {
-         	public void actionPerformed(ActionEvent arg0) {
-         		setTxt();
-         	}
-         });
-         panel.add(btnNewButton);
-         
-         JButton btnNewButton_1 = new JButton("read values");
-         btnNewButton_1.addActionListener(new ActionListener() {
-         	public void actionPerformed(ActionEvent arg0) {
-         		readTXT();
-                int m = Integer.parseInt(textField_M.getText());
-                int n = Integer.parseInt(textField_N.getText());
-                int x = Integer.parseInt(textField_X.getText().substring(0, textField_X.getText().length()-1));
-                data = new Data_Matrix(m, n, x);
-                data.print_data_matrix();
-                data.print_data_matrix_graphics(textPane);
-         	}
-         });
-         panel.add(btnNewButton_1);
-         setLayout(groupLayout);
-    	
-    	
-    	
+    
+    public static void createMatrix(){
+        data = new Data_Matrix(M, N, X);
+        data.print_data_matrix();
+        data.print_data_matrix_graphics(gui.textPane);
+        
+        data_predicted = new Data_Matrix(M, N, X);
+//        data_predicted.print_data_matrix();
+//        data_predicted.print_data_matrix_graphics(gui.textPane);
     }
+    
 }
-
-
 
 
