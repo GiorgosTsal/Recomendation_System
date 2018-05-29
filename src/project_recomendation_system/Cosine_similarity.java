dd608edc6bc37390b0e407818751e7f350305e03
Giorgos Tsalidis <tsalgiwrgos@gmail.com>
2018-05-29 04:04:53 +0300

    Add files via upload

---
+++ src/project_recomendation_system/Cosine_similarity.java
@@ -0,0 +1,29 @@
+package project_recomendation_system;
+
+public class Cosine_similarity {
+
+	public static Double compute(double[] vector1 , double[] vector2){
+		
+		if(vector1.length != vector2.length){
+			System.err.println("at computation of cosine similarity vectors are not the same length!");
+			return 0.0 ;
+		}
+		int length = vector1.length;
+		Double sum =  0.0;
+		Double sum1 = 0.0;
+		Double sum2 = 0.0;
+		
+		for(int index=0; index < length; index++){
+			if(vector1[index]!= -1 && vector2[index]!=-1){
+				sum1 = sum1 + vector1[index]*vector1[index];
+				sum2 = sum2 + vector2[index]*vector2[index];
+				sum  = sum  + vector1[index]*vector2[index];
+			}
+		
+		}
+		Double value = sum/(Math.sqrt(sum1)*Math.sqrt(sum2));
+		return value;
+		
+	}
+	
+}
